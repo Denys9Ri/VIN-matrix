8a882ee692604f0a1caefb8fc4bfdbb7319b3a43
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-05-04 20:18:45 +0300

    Add WSGI configuration for vin_matrix project

---
+++ backend/vin_matrix/wsgi.py
@@ -0,0 +1,10 @@
+"""
+WSGI config for vin_matrix project.
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vin_matrix.settings')
+
+application = get_wsgi_application()

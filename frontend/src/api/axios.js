037a10d9c57d4ddb32a6ede28abb283d3f6a4617
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-05-08 16:55:06 +0300

    Fix axios import statement in axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,9 @@
-import axios from 'react';
+import axios from 'axios'; 
 
 const api = axios.create({
-  // Ось тут ми підключили твій живий бекенд!
   baseURL: 'http://c7flj95csavoasntnnxolemw.95.217.211.207.sslip.io',
 });
 
-// Цей блок автоматично додає "перепустку" (токен) до кожного запиту
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('access_token');
   if (token) {

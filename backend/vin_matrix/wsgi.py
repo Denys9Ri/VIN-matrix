5ed7754a5bf293030eca010a44086aa427d6620e
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-05-09 09:15:39 +0300

    Modify WSGI for targeted database migrations
    Updated automatic migration commands to specifically target the 'core' app.

--- backend/vin_matrix/wsgi.py
+++ backend/vin_matrix/wsgi.py
@@ -3,14 +3,16 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vin_matrix.settings')
 
-# Це стандартний запуск сервера
 application = get_wsgi_application()
 
 # --- НАШ ХАКЕРСЬКИЙ БЛОК АВТОМІГРАЦІЙ ---
 from django.core.management import call_command
 try:
     print("Починаємо магію: автоматичне оновлення бази...")
-    call_command('makemigrations')
+    
+    # ПРИМУСОВО кажемо шукати таблиці в додатку core!
+    call_command('makemigrations', 'core') 
+    
     call_command('migrate')
     print("База даних успішно оновлена!")
 except Exception as e:

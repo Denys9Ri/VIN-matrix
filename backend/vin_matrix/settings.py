e7271083f6b73046c4bd61309cd20f257029a676
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-05-23 16:17:32 +0300

    Configure CORS and CSRF settings in settings.py

--- backend/vin_matrix/settings.py
+++ backend/vin_matrix/settings.py
@@ -145,3 +145,18 @@ TIME_ZONE = 'Europe/Kyiv'
 USE_TZ = True
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://*.95.217.211.207.sslip.io",
+    "https://*.95.217.211.207.sslip.io",
+]

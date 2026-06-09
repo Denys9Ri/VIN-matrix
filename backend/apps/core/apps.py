08a6be39ddfa624e917ee57be97a01b39601d4d0
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 09:13:40 +0300

    Make CoreConfig syntactically valid

--- backend/apps/core/apps.py
+++ backend/apps/core/apps.py
@@ -1,15 +1,12 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.core'
-    verbose_name = 'Базові сутності'
-
+    default_auto_field='django.db.models.BigAutoField'
+    name='apps.core'
     def ready(self):
         try:
             from django.urls import path
             import vin_matrix.urls as u
             from .novapost_views import NovaPostDeliveryCreateView
-            names = {getattr(x, 'name', None) for x in u.urlpatterns}
-            if 'novapost-del
+            names={getattr(x,'name',None) for x in u.urlpatterns}
+            routes

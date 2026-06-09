15ac6dae4e07b27e6223de827412410da56250fe
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 10:37:53 +0300

    Restore compact urls.py with TTN routes

--- backend/vin_matrix/urls.py
+++ backend/vin_matrix/urls.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import include, path, re_path
+from django.urls import include,path,re_path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from apps.core.novapost_views import NovaPostDeliveryCreateView
+from rest_framework_simplejwt import views as jwt_views
+from apps.core.views import RegisterView,VisitViewSet,ServiceCatalogViewSet,ProfileSettingsView,LogoutView,ChangePasswordView
+from apps.core.novapost

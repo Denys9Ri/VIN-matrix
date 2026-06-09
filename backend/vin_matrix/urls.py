0a595fd0af0d17c7769cf6ee3163abf1dab35b6c
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 10:43:14 +0300

    Restore valid urls.py shell

--- backend/vin_matrix/urls.py
+++ backend/vin_matrix/urls.py
@@ -1,9 +1,1 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.http import JsonResponse
-from django.urls import include,path,re_path
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt import views as jwt_views
-from apps.core.views import RegisterView,VisitViewSet,ServiceCatalogViewSet,ProfileSettingsView,LogoutView,ChangePasswordView
-from apps.core.novapost
+urlpatterns=[]

ff2dc08b2579e8d14bf9e6514b729a27d5c0142c
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 10:20:56 +0300

    Commit 6 step 1: add Nova Post TTN routes

--- backend/vin_matrix/urls.py
+++ backend/vin_matrix/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.http import JsonResponse
 from django.conf import settings
+from django.contrib import admin
+from django.http import JsonResponse
+from django.urls import include, path, re_path
 from django.views.static import serve
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
-from apps.core.views import LogoutView, ChangePasswordView
-from apps.core.safe_crm_views import VisitViewSet, ServiceCatalogViewSet, OrderPartViewSet, OrderServiceViewSet, VehicleRecommendationViewSet, CRMTaskViewSet
-from apps.core.communication_views import CRMCommunicationViewSet, CRMClientStatusViewSet, CRMServiceReminderView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from apps.core.views import ChangePasswordView, LogoutView
+from apps.core.safe_crm_views import CRMTaskViewSet, Order

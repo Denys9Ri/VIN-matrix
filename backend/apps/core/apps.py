a8d5aa7f2a5a317415dec8583f698a707afb42f8
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 13:11:49 +0300

    Restore valid core app config

--- backend/apps/core/apps.py
+++ backend/apps/core/apps.py
@@ -6,10 +6,4 @@ class CoreConfig(AppConfig):
     name = 'apps.core'
 
     def ready(self):
-        try:
-            from django.urls import path
-            from importlib import import_module
-            from .billing_client_link_views import BillingAdminClientLinkView
-
-            urlconf = import_module('vin_matrix.urls')
-            route =
+        return None

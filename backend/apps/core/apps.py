8a73c524e0d55ae5d32219a286ed66e6ec21f20e
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 09:18:08 +0300

    Restore valid CoreConfig

--- backend/apps/core/apps.py
+++ backend/apps/core/apps.py
@@ -1,12 +1,8 @@
 from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
-    default_auto_field='django.db.models.BigAutoField'
-    name='apps.core'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.core'
+
     def ready(self):
-        from django.urls import path
-        import vin_matrix.urls as u
-        from .novapost_views import NovaPostDeliveryCreateView as V
-        n={getattr(x,'name',None) for x in u.urlpatterns}
-        if 'novapost-delivery-create-ttn' not in n:
-            u.urlpatterns.append
+        return None

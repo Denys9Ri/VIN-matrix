a5a0489db3092925a4e5b8f6b8f4e77e76eeef20
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 10:24:30 +0300

    Emergency restore TTN routes

--- backend/vin_matrix/urls.py
+++ backend/vin_matrix/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
-from django.http import JsonResponse
-from django.urls import path,re_path
-from apps.core.novapost_views import NovaPostDeliveryCreateView
-
-def api_root(request): return JsonResponse({'message':'VIN-matrix API is running!'})
-urlpatterns=[path('',api_root),path('admin/',admin.site.urls),re_path(r'^api/delivery/novapost/visits/(?P<visit_id>\d+)/create-ttn/$',
+from django.urls import re_path
+from apps.core.novapost_views import NovaPostDeliveryCreateView as V
+urlpatterns=[
+re_path(r'^api/delivery/novapost/visits/(?P<visit_id>\d+)/create-ttn/$',V.as_view()),
+re_path(r'^api/delivery/novapost/visits/(?P<visit_id>\d+)/create/$',V.as_view())]

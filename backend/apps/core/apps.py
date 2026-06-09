eb60be3ed079465500cf29ac7c880906430f40a7
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 09:14:59 +0300

    Repair truncated CoreConfig

--- backend/apps/core/apps.py
+++ backend/apps/core/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
-    default_auto_field='django.db.models.BigAutoField'
-    name='apps.core'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.core'
+    verbose_name = 'Базові сутності'
+
     def ready(self):
-        try:
-            from django.urls import path
-            import vin_matrix.urls as u
-            from .novapost_views import NovaPostDeliveryCreateView
-            names={getattr(x,'name',None) for x in u.urlpatterns}
-            routes
+        pass

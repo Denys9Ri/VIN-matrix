cb95a8a11c87fb086e3f94a1a7db8bc8a266711e
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-09 12:50:03 +0300

    Restore full API URL configuration

--- backend/vin_matrix/urls.py
+++ backend/vin_matrix/urls.py
@@ -1,10 +1,17 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.http import JsonResponse
 from django.conf import settings
+from django.contrib import admin
+from django.http import JsonResponse
+from django.urls import include, path, re_path
 from django.views.static import serve
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
-
-from apps.core.views import LogoutView, ChangePasswordView
-from apps.core.safe_crm_views import VisitViewSet, ServiceCatalogViewSet, OrderPartViewSet, OrderServiceViewSet, Vehicle
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.core.views import ChangePasswordView, LogoutView
+from apps.core.platform_auth_views import RegisterView
+from apps.core.profile_views import ProfileSettingsView
+from apps.core.safe_crm_views import CRMTaskViewSet, OrderPartViewSet, OrderServiceViewSet, ServiceCatalogViewSet, VehicleRecommendationViewSet, VisitViewSet
+from apps.core.communication_views import CRMClientStatusViewSet, CRMCommunicationViewSet, CRMServiceReminderViewSet
+from apps.core.visit_workflow_views import VisitAcceptanceActView, VisitDiagnosticChecklistView
+from apps.core.ocr_views import RecognizeDocumentView
+from apps.core.complex_views import ServiceComplexViewSet
+from apps.core.stock_views import StockMovementViewSet, StockReceiveViewSet
+from apps.core

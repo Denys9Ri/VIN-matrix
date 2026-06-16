36928718cc9d3a60844025d0439bc637f9281e3b
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-16 19:30:40 +0300

    Enable billing access middleware

--- backend/vin_matrix/settings.py
+++ backend/vin_matrix/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'apps.core.billing_access_middleware.BillingAccessMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]

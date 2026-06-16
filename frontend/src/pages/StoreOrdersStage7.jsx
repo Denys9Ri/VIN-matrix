a779c6af1e3f80190d5eea9affef160a046e5645
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-16 18:53:24 +0300

    Remove intrusive floating Nova Post panel

--- frontend/src/pages/StoreOrdersStage7.jsx
+++ frontend/src/pages/StoreOrdersStage7.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import StoreOrdersDictionaryBoard from './StoreOrdersDictionaryBoard.jsx';
-import NovaPostDeliveryControl from '../components/delivery/NovaPostDeliveryControl';
 
 export default function StoreOrdersStage7() {
-  return (
-    <>
-      <StoreOrdersDictionaryBoard />
-      <NovaPostDeliveryControl />
-    </>
-  );
+  return <StoreOrdersDictionaryBoard />;
 }

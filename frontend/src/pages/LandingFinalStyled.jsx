ee53264a7d9a5b58e4ca54ca519ce484e2f1d793
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-21 17:10:38 +0300

    Wire final landing visuals into public routes

---
+++ frontend/src/pages/LandingFinalStyled.jsx
@@ -0,0 +1,13 @@
+import FinalLanding, { FinalDemoTour } from './LandingFinal';
+import './FinalBase.css';
+import './FinalFrame.css';
+import './FinalScenesVisits.css';
+import './FinalScenesData.css';
+
+export function DemoTour() {
+  return <FinalDemoTour />;
+}
+
+export default function Landing() {
+  return <FinalLanding />;
+}

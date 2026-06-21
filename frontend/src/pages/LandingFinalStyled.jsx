536a3078ecb58096acaf6cf7cf484b5b421176cb
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-21 17:17:24 +0300

    Load final marketing style layers

--- frontend/src/pages/LandingFinalStyled.jsx
+++ frontend/src/pages/LandingFinalStyled.jsx
@@ -3,6 +3,7 @@ import './FinalBase.css';
 import './FinalFrame.css';
 import './FinalScenesVisits.css';
 import './FinalScenesData.css';
+import './FinalMarketingAll.css';
 
 export function DemoTour() {
   return <FinalDemoTour />;

69e622ee2e3bfcb0b3d42f70deab7aefe7615059
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-06-28 19:43:22 +0300

    Add schema merge

---
+++ backend/apps/agent/migrations/0003_merge_agent_write_defaults.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('agent', '0002_rename_agent_indexes'),
+        ('agent', '0002_disable_write_confirmation_default'),
+    ]
+    operations = []

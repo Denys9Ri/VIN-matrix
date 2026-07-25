f7b98eda083cad2d0c3918c01926e532a9668010
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-07-25 11:44:57 +0300

    Configure landing growth app

---
+++ backend/apps/landing_growth/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class LandingGrowthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.landing_growth'
+    verbose_name = 'Landing Growth Engine'

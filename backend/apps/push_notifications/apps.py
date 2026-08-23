fec513e0eed8e619cd8e214c3ca566fa7cf79e15
Denys Riabuha <104528122+Denys9Ri@users.noreply.github.com>
2026-08-23 15:27:58 +0300

    Add push notifications app config

---
+++ backend/apps/push_notifications/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PushNotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.push_notifications'
+    verbose_name = 'Push notifications'
